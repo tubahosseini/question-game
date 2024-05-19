d55f1679d582e17f376c655ec9b4bc91138b0f8b
Tuba Hosseini <tubahosseini@gmail.com>
2024-05-19 16:25:50 +0330

    initial commit

---
+++ src/pages/home-page/HomePage.tsx
@@ -0,0 +1,9 @@
+import HomeComponent from "../../components/home-component/HomeComponent";
+
+export default function HomePage() {
+  return (
+    <div>
+      <HomeComponent />
+    </div>
+  );
+}
