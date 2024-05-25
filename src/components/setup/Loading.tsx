8b1f2d9629cc09746395c51ba284b13eec390f5d
Tuba Hosseini <tubahosseini@gmail.com>
2024-05-26 00:03:47 +0330

    Loading added

---
+++ src/components/setup/Loading.tsx
@@ -0,0 +1,12 @@
+// this code is from https://tailwindflex.com/@jaxstone/loding-spinner-with-image-inside
+export default function Loading() {
+  return (
+    <div className="relative flex justify-center items-center">
+      <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-[#2e1437b5]"></div>
+      <img
+        src="https://www.svgrepo.com/show/509001/avatar-thinking-9.svg"
+        className="rounded-full h-28 w-28"
+      />
+    </div>
+  );
+}
--- src/components/setup/Setup.tsx
+++ src/components/setup/Setup.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { useData } from "../../context/DataContext";
 import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
 
 export interface dataType {
   category: string;
@@ -18,25 +19,32 @@ export default function Setup() {
   const [category, setCategory] = useState("");
   const [difficulty, setDifficulty] = useState("");
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
-  async function getData() {
-    const response = await axios.get(
-      `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`
-    );
-    setData(response.data.results);
-    console.log(response.data.results[0]);
-    setAmount("");
-    setCategory("");
-    setDifficulty("");
-  }
 
   const start = async () => {
     if (validate()) {
+      setLoading(true);
       await getData();
       navigate("/quiz");
+      setLoading(false);
     }
   };
+
+  async function getData() {
+    try {
+      const response = await axios.get(
+        `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`
+      );
+      setData(response.data.results);
+      setAmount("");
+      setCategory("");
+      setDifficulty("");
+    } catch (error) {
+      console.error("Error fetching data", error);
+      setLoading(false); // Reset loading in case of error
+    }
+  }
 
   const validate = () => {
     let tempErrors: { [key: string]: string } = {};
@@ -60,70 +68,77 @@ export default function Setup() {
     return Object.keys(tempErrors).length === 0;
   };
 
-  //test
-  console.log(data);
+  console.log(data); //test
 
   return (
     <div className="flex items-center justify-center h-screen px-4 md:px-8 lg:px-16">
       <div className="w-full max-w-lg bg-white text-[#262626] rounded-xl py-8 px-6 md:py-12 md:px-10 lg:py-16 lg:px-16 flex flex-col gap-5">
-        <h1 className="text-3xl md:text-4xl text-center font-serif">
-          Setup Quiz
-        </h1>
-        <hr className="border-none h-[2px] bg-[#707070]" />
-        <div className="mt-4">
-          <p className="font-semibold">Number of questions:</p>
-          <input
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            className="w-full outline-none border p-1 mt-1"
-            type="number"
-            placeholder="Between 5 and 10"
-          />
-          {errors.amount && (
-            <p className="text-red-500 text-sm mt-1">{errors.amount}</p>
-          )}
-        </div>
-        <div>
-          <p className="font-semibold">Category :</p>
-          <select
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="w-full p-1 border mt-1 outline-none"
-            name="category"
-          >
-            <option value="">category</option>
-            <option value="10">Books</option>
-            <option value="21">Sport</option>
-            <option value="27">Animals</option>
-          </select>
-          {errors.category && (
-            <p className="text-red-500 text-sm mt-1">{errors.category}</p>
-          )}
-        </div>
-        <div>
-          <p className="font-semibold">Difficulty :</p>
-          <select
-            value={difficulty}
-            onChange={(e) => setDifficulty(e.target.value)}
-            className="w-full p-1 border mt-1 outline-none"
-            name="category"
-          >
-            <option value="">difficulty</option>
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
-          </select>
-          {errors.difficulty && (
-            <p className="text-red-500 text-sm mt-1">{errors.difficulty}</p>
-          )}
-        </div>
-        <button
-          onClick={start}
-          type="submit"
-          className="glow-on-hover bg-[#2E1437] hover:bg-[#2e1437dc] text-white m-auto w-full max-w-[150px] h-[50px] font-semibold rounded-md"
-        >
-          Start
-        </button>
+        {loading ? (
+          <div className="flex items-center justify-center h-1/2">
+            <Loading />
+          </div>
+        ) : (
+          <>
+            <h1 className="text-3xl md:text-4xl text-center font-serif">
+              Setup Quiz
+            </h1>
+            <hr className="border-none h-[2px] bg-[#707070]" />
+            <div className="mt-4">
+              <p className="font-semibold">Number of questions:</p>
+              <input
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                className="w-full outline-none border p-1 mt-1"
+                type="number"
+                placeholder="Between 5 and 10"
+              />
+              {errors.amount && (
+                <p className="text-red-500 text-sm mt-1">{errors.amount}</p>
+              )}
+            </div>
+            <div>
+              <p className="font-semibold">Category :</p>
+              <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                className="w-full p-1 border mt-1 outline-none"
+                name="category"
+              >
+                <option value="">category</option>
+                <option value="10">Books</option>
+                <option value="21">Sport</option>
+                <option value="27">Animals</option>
+              </select>
+              {errors.category && (
+                <p className="text-red-500 text-sm mt-1">{errors.category}</p>
+              )}
+            </div>
+            <div>
+              <p className="font-semibold">Difficulty :</p>
+              <select
+                value={difficulty}
+                onChange={(e) => setDifficulty(e.target.value)}
+                className="w-full p-1 border mt-1 outline-none"
+                name="category"
+              >
+                <option value="">difficulty</option>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+              {errors.difficulty && (
+                <p className="text-red-500 text-sm mt-1">{errors.difficulty}</p>
+              )}
+            </div>
+            <button
+              onClick={start}
+              type="submit"
+              className="bg-[#2E1437] hover:bg-[#2e1437dc] text-white m-auto w-full max-w-[150px] h-[50px] font-semibold rounded-md"
+            >
+              Start
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
